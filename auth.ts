f40248ce18d149981b5e8baa6422f17c0e501ce3
Yousepha <yousane99@gmail.com>
2025-04-14 23:48:38 +0000

    auth-middleware

--- auth.ts
+++ auth.ts
@@ -1,5 +1,19 @@
-import NextAuth from "next-auth"
+// app/auth.ts
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github"; // Exemple
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
+
+
+// import NextAuth from "next-auth"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [],
+// })
