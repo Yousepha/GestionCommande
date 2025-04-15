6d4db082f46b89db96b3a79c4ffa130f29114750
Yousepha <yousane99@gmail.com>
2025-04-15 01:28:19 +0000

    CLIdelFiletest3

--- app/api/clients/[id]/route.ts
+++ app/api/clients/[id]/route.ts
@@ -1,40 +1,66 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import type { Client } from "@prisma/client";
 import prisma from "@/app/lib/prisma";
-import type { NextRequest } from "next/server";
 
-export const PATCH = async (request: Request, {params}: {params: {id: string}}) =>{
-    const body: Client = await request.json();
-    const client = await prisma.client.update({
-        where:{
-            idClient: Number(params.id)
-        },
-        data:{
-            prenom: body.prenom,
-            nom: body.nom,
-            adresse: body.adresse,
-            telephone: body.telephone,
-            entreprise: body.entreprise
-        }
-    });
-    return NextResponse.json(client, {status: 200});
+// PATCH (update client)
+export async function PATCH(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const body: Client = await request.json();
+
+  const updatedClient = await prisma.client.update({
+    where: {
+      idClient: Number(context.params.id),
+    },
+    data: {
+      prenom: body.prenom,
+      nom: body.nom,
+      adresse: body.adresse,
+      telephone: body.telephone,
+      entreprise: body.entreprise,
+    },
+  });
+
+  return NextResponse.json(updatedClient, { status: 200 });
 }
 
+// DELETE (delete client)
 export async function DELETE(
-    request: NextRequest,
-    context: { params: { id: string } }
-  ) {
-    const clientId = Number(context.params.id);
-  
-    const deletedClient = await prisma.client.delete({
-      where: {
-        idClient: clientId,
-      },
-    });
-  
-    return NextResponse.json(deletedClient, { status: 200 });
-  }
-  
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const deletedClient = await prisma.client.delete({
+    where: {
+      idClient: Number(context.params.id),
+    },
+  });
+
+  return NextResponse.json(deletedClient, { status: 200 });
+}
+
+
+// import { NextResponse } from "next/server";
+// import type { Client } from "@prisma/client";
+// import prisma from "@/app/lib/prisma";
+
+// export const PATCH = async (request: Request, {params}: {params: {id: string}}) =>{
+//     const body: Client = await request.json();
+//     const client = await prisma.client.update({
+//         where:{
+//             idClient: Number(params.id)
+//         },
+//         data:{
+//             prenom: body.prenom,
+//             nom: body.nom,
+//             adresse: body.adresse,
+//             telephone: body.telephone,
+//             entreprise: body.entreprise
+//         }
+//     });
+//     return NextResponse.json(client, {status: 200});
+// }
 
 // export const DELETE = async (request: Request, {params}: {params: {id: string}}) =>{
 //     const client = await prisma.client.delete({
